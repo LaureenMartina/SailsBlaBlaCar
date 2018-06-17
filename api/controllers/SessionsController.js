ff1457e7702443f6d9056db46cdda237c16d7ce9
Lays <L.martina@protonmail.com>
2018-06-17 16:05:50 +0200

    add sessionController

--- api/controllers/SessionsController.js
+++ api/controllers/SessionsController.js
@@ -7,14 +7,6 @@
 var bcrypt = require('bcrypt');
 module.exports = {
 
-  'new': function(req,res){
-    var oldDateObj = new Date();
-    var newDateObj = new Date(oldDateObj.getTime() + 60000 );
-    req.session.cookie.expires = newDateObj;
-    req.session.authenticated = true;
-    console.log(req.session);
-  },
-
   create: async function(req, res, next){
     if (!req.param('username') || !req.param('password')) {
       var usernamePasswordRequiredError = [{name: 'usernamePasswordRequired', message: 'Il faut saisir le pseudo et le mot de passe'}];
@@ -33,6 +25,8 @@ module.exports = {
         var usernamePasswordMismatchError = [{name: 'usernamePasswordMismatchError', message: 'Le mot de passe incorrect'}]
         return next(res.json(usernamePasswordMismatchError));
       }else{
+        req.session.authenticated = true;
+        req.session.User = authUser;
         return next(res.ok());
       }
       return next(res.json(valid));
@@ -41,6 +35,6 @@ module.exports = {
 
   destroy: function(req, res, next){
     req.session.destroy();
-    return("Vous êtes déconnecté");
+    return req.session.destroy();
   }
 };
