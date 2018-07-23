98e83d600a21631fc79fb8754186c6fdc298707c
Lays <L.martina@protonmail.com>
2018-07-23 23:03:21 +0200

    auto-indent

--- api/models/Cars.js
+++ api/models/Cars.js
@@ -1,33 +1,33 @@
 /**
- * Cars.js
- *
- * @description :: A model definition.  Represents a database table/collection/etc.
- * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
- */
+* Cars.js
+*
+* @description :: A model definition.  Represents a database table/collection/etc.
+* @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+*/
 
 module.exports = {
 
-  attributes: {
-    model: {
-        type: 'String',
-        defaultsTo: 'Lambda'
+    attributes: {
+        model: {
+            type: 'String',
+            defaultsTo: 'Lambda'
+        },
+        color: {
+            type: 'String',
+            defaultsTo: 'White'
+        },
+        seats: {
+            type: 'Number',
+            defaultsTo: 2
+        },
+        owner: {
+            model: 'User',
+            required: true
+        },
+        trips: {
+            collection: 'Trip',
+            via: 'car'
+        }
     },
-    color: {
-        type: 'String',
-        defaultsTo: 'White'
-    },
-    seats: {
-        type: 'Number',
-        defaultsTo: 2
-    },
-    owner: {
-        model: 'User',
-        required: true
-    },
-    trips: {
-      collection: 'Trip',
-      via: 'car'
-    }
-  },
 
 };
--- api/models/Trip.js
+++ api/models/Trip.js
@@ -1,35 +1,35 @@
 /**
- * Trip.js
- *
- * @description :: A model definition.  Represents a database table/collection/etc.
- * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
- */
+* Trip.js
+*
+* @description :: A model definition.  Represents a database table/collection/etc.
+* @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+*/
 
 module.exports = {
 
-  attributes: {
-    start: {
-        type: 'String',
-        required: true
+    attributes: {
+        start: {
+            type: 'String',
+            required: true
+        },
+        end: {
+            type: 'String',
+            required: true
+        },
+        status: {
+            type: 'Number',
+            defaultsTo: 0
+        },
+        participants: {
+            collection: 'User'
+        },
+        driver: {
+            model: 'User',
+            required: true
+        },
+        car: {
+            model: 'Cars'
+        }
     },
-    end: {
-        type: 'String',
-        required: true
-    },
-    status: {
-        type: 'Number',
-        defaultsTo: 0
-    },
-    participants: {
-        collection: 'User'
-    },
-    driver: {
-        model: 'User',
-        required: true
-    },
-    car: {
-        model: 'Cars'
-    }
-  },
 
 };
